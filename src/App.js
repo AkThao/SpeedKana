56753ed69b5227c307141d0d27c4e8df14d3944d
AkThao <akaash.thao@gmail.com>
2021-09-12 12:39:13 +0100

    Add state to allow switching between app views

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import React, { useState } from "react";
 import "./App.css";
 import MainView from "./components/MainView/MainView";
 import TestView from "./components/TestView/TestView";
 
+const APP_VIEWS = {
+    MAIN: "main",
+    TEST: "test",
+    STATS: "stats",
+    SETTINGS: "settings"
+};
+
 function App() {
-    return (
-        <div className="app">
-            {/* Display either MainView or TestView based on appState */}
-            {/* <MainView /> */}
-            <TestView />
-        </div>
-    );
+    const [appView, setAppView] = useState(APP_VIEWS.MAIN);
+
+    switch(appView) {
+        case APP_VIEWS.MAIN:
+            return <MainView />
+        case APP_VIEWS.TEST:
+            return <TestView />
+        default:
+            return <MainView />
+    }
 }
 
 export default App;
