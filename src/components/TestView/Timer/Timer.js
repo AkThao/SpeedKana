51998afabd9b2d8b0db785d7aeffcd165103f7e8
AkThao <akaash.thao@gmail.com>
2021-10-14 19:06:30 +0100

    Display minutes and seconds in test timer

--- src/components/TestView/Timer/Timer.js
+++ src/components/TestView/Timer/Timer.js
@@ -1,7 +1,11 @@
 const Timer = props => {
+    let time = props.time;
+    let minutes = (time - (time % 60)) / 60;
+    let seconds = time % 60;
+
     return (
         <div>
-            Timer: {props.time}
+            Timer: {(minutes).toLocaleString("en-GB", {minimumIntegerDigits: 2, useGrouping: false})}:{(seconds).toLocaleString("en-GB", {minimumIntegerDigits: 2, useGrouping: false})}
         </div>
     );
 };
