76139e0d2c76419d5e685b1a8e6245ec212a224a
AkThao <akaash.thao@gmail.com>
2022-04-13 21:20:54 +0100

    Implement test mode

--- src/components/TestView/TestLogic/TestLogic.js
+++ src/components/TestView/TestLogic/TestLogic.js
@@ -6,23 +6,89 @@ import ProgressBar from "../ProgressBar/ProgressBar";
 import Timer from "../Timer/Timer";
 import PauseButton from "../PauseButton/PauseButton";
 import EndButton from "../EndButton/EndButton";
+import kana from "../../../kana.json";
 
 const Test = props => {
     const [inputAnswer, setInputAnswer] = useState("");
     const [timeElapsed, setTimeElapsed] = useState(0);
-
+    const [complete, setComplete] = useState(false);
+    const [disabledInput, setDisabledInput] = useState(false);
+    const [testChar, setTestChar] = useState("");
+    const [correctAnswers, setCorrectAnswers] = useState(0);
+    const [incorrectAnswers, setIncorrectAnswers] = useState(0);
+    const [remainingChars, setRemainingChars] = useState(null);
+    const testSet = useRef({});
     const timerIntervalRef = useRef(0);
     const startTimeRef = useRef(Date.now());
 
-    const changeAnswer = newAnswer => {
+    const changeAnswer = (newAnswer) => {
         setInputAnswer(newAnswer);
     };
 
+    const submitAnswer = () => {
+        checkAnswer();
+    }
+
+    const checkAnswer = () => {
+        if (inputAnswer === testSet.current[testChar]) {
+            setCorrectAnswers((prev) => (prev + 1));
+        }
+        else {
+            setIncorrectAnswers((prev) => (prev + 1));
+        }
+        nextQuestion();
+    }
+
+    const nextQuestion = () => {
+        setInputAnswer("");
+        delete testSet.current[testChar];
+        setRemainingChars(Object.keys(testSet.current).length);
+        pickTestChar();
+    }
+
+    const pickTestChar = () => {
+        // Pick random key from testSet object
+        let keys = Object.keys(testSet.current); // keys is an array
+        if (keys.length === 0) {
+            completeTest();
+        }
+        else {
+            setTestChar(() => (
+                keys[keys.length * Math.random() << 0] // The << (left-shift) operator fixes the index by coercing the floating-point operand into a 32-bit integer
+            ))
+        }
+    }
+
+    const completeTest = () => {
+        setComplete(true);
+        clearInterval(timerIntervalRef.current);
+        setTestChar("");
+        setDisabledInput(true);
+    }
+
     useEffect(() => {
+        // Create test timer
         timerIntervalRef.current = setInterval(() => {
             let delta = (Date.now() - startTimeRef.current) / 1000;
             setTimeElapsed(Math.floor(delta));
         }, 1000);
+
+        // Create test set - will eventually use props to set the test mode and customise the test set
+        // But for now the default set is all Kana
+        testSet.current = {
+            ...kana["hiragana"],
+            // ...kana["hiragana-diacritics"],
+            // ...kana["hiragana-digraphs"],
+            // ...kana["katakana"],
+            // ...kana["katakana-diacritics"],
+            // ...kana["katakana-digraphs"]
+        }
+
+        let keys = Object.keys(testSet.current); // keys is an array
+        setTestChar(() => (
+            keys[keys.length * Math.random() << 0] // The << (left-shift) operator fixes the index by coercing the floating-point input into a 32-bit integer
+        ))
+        setRemainingChars(keys.length);
 
         return () => {
             clearInterval(timerIntervalRef.current);
@@ -33,11 +99,21 @@ const Test = props => {
         <div>
             <TestCharacter />
             <CorrectCharacter />
-            <Input changeAnswer={changeAnswer} />
+            <Input answer={inputAnswer} changeAnswer={changeAnswer} submitAnswer={submitAnswer} disabled={disabledInput} />
             <ProgressBar />
             <Timer time={timeElapsed} />
             <PauseButton />
             <EndButton onClick={props.changeAppView} />
+            <div>{testChar}</div>
+            <div>{inputAnswer}</div>
+            <div>Remaining: {remainingChars}</div>
+            <div>Correct: {correctAnswers}</div>
+            <div>Incorrect: {incorrectAnswers}</div>
+            {
+                complete
+                ? <div>Test complete!</div>
+                : null
+            }
         </div>
     );
 };
