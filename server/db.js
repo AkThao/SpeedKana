52cb880a43389c3c7803a9534d735925070a200f
AkThao <akaash.thao@gmail.com>
2022-06-21 21:13:21 +0100

    Create database and stats table

---
+++ server/db.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const sqlite3 = require("sqlite3");
+
+// Get the location of the database file
+const dbPath = path.resolve(__dirname, "./db/database.sqlite");
+
+// Create a connection to the SQLite database
+const db = new sqlite3.Database(dbPath, err => {
+    if (err) {
+        console.error("Could not connect to database", err);
+    } else {
+        console.log("Connected to database");
+    }
+});
+
+// SQL code to create a table to store test stats
+const createStatsTable = `
+create table if not exists stats (
+    id integer primary key autoincrement,
+    test_num integer,
+    num_correct integer,
+    num_incorrect integer,
+    total_questions integer,
+    total_time integer,
+    date_time datetime
+);`
+
+// Create stats table
+db.run(createStatsTable, err => {
+    if (err) {
+        console.error("Could not create stats table", err);
+    } else {
+        console.log("Created stats table");
+    }
+});
+
+// Log all data in the table, just for shits and giggles
+db.all("select * from stats", (err, stats) => {
+    if (err) {
+        console.error("Could not get stats", err);
+    } else {
+        console.log(stats);
+    }
+});
+
+// Export the database connection
+module.exports = db;
