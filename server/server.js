6eaa3b16e60d288666cc831b0af602fe0efd6898
AkThao <akaash.thao@gmail.com>
2022-06-29 20:04:02 +0100

    Small improvements on server

--- server/server.js
+++ server/server.js
@@ -2,22 +2,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+// Create express app
 const app = express();
+
+// Set default port for express app
 const PORT = process.env.PORT || 4000;
 
-app.use(cors({
-    origin: "http://localhost:3000"
-}));
+// Apply middleware - this should stay above routes
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Default Hello World route because why not
 app.get("/api/hello", (req, res) => {
     res.send({ message: "Hello World!" });
 });
 
-app.post("/api/world", (req, res) => {
-    console.log(req.body);
-    res.send(`Got a POST request, the message was: ${req.body.post}`);
+// Implement 500 error route
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something went wrong on the server.");
+});
+
+// Implement 404 error route
+app.use((err, req, res, next) => {
+    res.status(404).send("Sorry, that route doesn't exist.");
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
