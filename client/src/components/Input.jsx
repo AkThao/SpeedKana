fa8b2c82bead462b8c0a25568fe821bf7efcb56a
AkThao <akaash.thao@gmail.com>
2022-11-21 17:39:31 +0000

    Style input field

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,4 +1,9 @@
+import { TextField } from "@mui/material";
+import { useTheme } from "@mui/material";
+
 const Input = props => {
+    const theme = useTheme();
+
     const handleChange = (e) => {
         props.changeAnswer(e.target.value);
     }
@@ -11,7 +16,16 @@ const Input = props => {
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" value={props.answer} onChange={handleChange} disabled={props.isTestComplete} />
+                <TextField sx={{
+                    backgroundColor: theme.palette.primary.grey,
+                    borderRadius: "6px",
+                    border: "2px solid black",
+                    input: {
+                        color: theme.palette.general.white,
+                        fontSize: "20px",
+                        padding: "10px 16px",
+                    }
+                }} type="text" placeholder="Type answer here..." variant="filled" color="primary" value={props.answer} onChange={handleChange} disabled={props.isTestComplete} />
             </form>
         </div>
     );
